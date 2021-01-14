504d12646f984936c1df6115259708e0fba48240
Ideklo <77453748+Ideklo@users.noreply.github.com>
2021-01-14 22:52:43 +0000

    SafeWord
    VS FILE

---
+++ SafeWord Update 1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Safeword_RESET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("What Do You Want Your Username To Be");
+            String userName = Console.ReadLine();
+            Console.WriteLine("Hello " + userName);
+
+            Console.WriteLine("Welcome To SafeWord!");
+            Console.WriteLine("The Most Safe Place To Keep your passwords nice and secure in a fancy place");
+            Console.Title = "Safeword";
+           
+            Console.WindowHeight = 40;
+            Console.WriteLine("Please Put In 3 Passwords");
+            Console.ReadLine();
+            Console.ReadLine();
+            Console.ReadLine();
+            Console.WriteLine("Please Put In Another 3 Passwords");
+            Console.ReadLine();
+            Console.ReadLine();
+            Console.ReadLine();
+            Console.WriteLine("This Is The Last Password Inject As Im Running Out Of Time More Updates On My GitHub");
+                    
+            Console.WriteLine("If You Would Like To Support Me Here Is My Paypal paypal.me/BMuszysky EMAIL:mewplayr01@gmail.com");
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
